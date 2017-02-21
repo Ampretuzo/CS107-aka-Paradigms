d0a93bf0d0b1d8a60354736db3b27a98c41d6590
Aro Atelli <arotell@udabno.com>
2017-02-21 14:12:50 +0400

    HashSetNew done.

--- assn-3-vector-hashset/hashset.c
+++ assn-3-vector-hashset/hashset.c
@@ -3,9 +3,52 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+#define HEURISTIC_VEC_SIZE 4
+
+/* helpers */
+
+static void assertHashSetValid(hashset* h)
+{
+  assert(h->elemSize > 0);
+  assert(h->numBuckets > 0);
+  assert(h->hashfn != NULL);
+  assert(h->comparefn != NULL);
+  // note that if elements do not contain pointers to dinamically
+  // allocated memory, h->freefn would be NULL
+  assert(h->start != NULL); // assert calloc was successful
+}
+ 
+static void initVectors(hashset* h)
+{
+  for(int i = 0; i < h->numBuckets; i++)
+    VectorNew(
+      (void*) ((char*) h->start + i * sizeof(vector)), 
+      h->elemSize, 
+      h->freefn, 
+      HEURISTIC_VEC_SIZE
+    );
+}
+ 
+ 
+/* helpers end */
+
+
 void HashSetNew(hashset *h, int elemSize, int numBuckets,
-		HashSetHashFunction hashfn, HashSetCompareFunction comparefn, HashSetFreeFunction freefn)
-{}
+		HashSetHashFunction hashfn, HashSetCompareFunction comparefn, 
+		HashSetFreeFunction freefn)
+{
+  h->elemSize = elemSize;
+  h->numBuckets = numBuckets;
+  h->hashfn = hashfn;
+  h->comparefn = comparefn;
+  h->freefn = freefn;
+  // vector array
+  h->start = calloc(h->numBuckets, sizeof(vector) );
+  assertHashSetValid(h);
+  // without too much super-optimization, init all vectors
+  initVectors(h);
+}
 
 void HashSetDispose(hashset *h)
 {}
